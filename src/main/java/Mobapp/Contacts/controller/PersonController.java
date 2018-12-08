d38f6dce5510b6221de2e8d6fea4d946bc9da3f6
hemughk <hemughk@gmail.com>
2018-12-08 17:05:58 +0530

    Signed-off-by: hemughk <hemughk@gmail.com>

--- src/main/java/Mobapp/Contacts/controller/PersonController.java
+++ src/main/java/Mobapp/Contacts/controller/PersonController.java
@@ -48,42 +48,6 @@ public class PersonController {
 		this.personService = ps;
 	}
 	
-	@RequestMapping(value = "/persons", method = RequestMethod.GET)
-	public String listPersons(Model model) {
-		model.addAttribute("person", new Person());
-		model.addAttribute("listPersons", this.personService.listPersons());
-		return "person";
-	}
-	
-	//For add and update person both
-	@RequestMapping(value= "/person/add", method = RequestMethod.POST)
-	public String addPerson(@ModelAttribute("person") Person p){
-		
-		if(p.getId() == 0){
-			//new person, add it
-			this.personService.addPerson(p);
-		}else{
-			//existing person, call update
-			this.personService.updatePerson(p);
-		}
-		
-		return "redirect:/persons";
-		
-	}
-	
-	@RequestMapping("/remove/{id}")
-    public String removePerson(@PathVariable("id") int id){
-		
-        this.personService.removePerson(id);
-        return "redirect:/persons";
-    }
- 
-    @RequestMapping("/edit/{id}")
-    public String editPerson(@PathVariable("id") int id, Model model){
-        model.addAttribute("person", this.personService.getPersonById(id));
-        model.addAttribute("listPersons", this.personService.listPersons());
-        return "person";
-    }
     
     @RequestMapping(value="/saveUinfo",method = RequestMethod.POST,
 			consumes="application/json",produces="application/json")
